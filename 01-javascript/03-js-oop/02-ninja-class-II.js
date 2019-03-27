4b5eb0b167d1072e55f23bc627674966160e19e4
Albiona <albionahh@outlook.com>
2019-03-27 14:44:45 +0100

    add js stroll - ninja update

--- 01-javascript/03-js-oop/02-ninja-class-II.js
+++ 01-javascript/03-js-oop/02-ninja-class-II.js
@@ -3,29 +3,34 @@ function Ninja(name) {
   this.name = name;
   this.health = 100;
   var speed = 3;
-  var strength = 1;
+  var strength = 3;
 
   this.sayName = function () { console.log("My ninja name is " + name); }
 
-  this.showStats = function () { console.log("Name: " + this.name + ", Health: " + this.health + ", Speed: " + speed + ", Strength: " + strength); }
+  this.showStats = function () { console.log("Name: " + this.name + ", Health: " + this.health + ", Speed: " + speed + ", Strength: ", strength); }
 
   this.drinkSake = function () {
     this.health += 10;
     return this;
   }
 
-  this.punch = function (ninja) {
-    if (ninja instanceof Ninja) {
-      ninja.health -= 5;
-      console.log(ninja.name + " was punched by " + this.name + " and lost 5 Health!");
+  this.punch = function (oneNinja) {
+    if (oneNinja instanceof Ninja) {
+      oneNinja.health -= 5;
+      console.log(this.name + " was punched by " + oneNinja.name + " and lost 5 Health!");
     }
     return this;
   }
 
-  this.kick = function (Ninja) {
-    if (Ninja instanceof Object) {
-      Ninja.health -= strength * 15;
-      console.log(Ninja.name + " was kicked by " + this.name + " and lost 15 Health");
+  this.kick = function (callNinja) {
+    if (callNinja instanceof Object) {
+      this.punch(callNinja);
+      let points = callNinja.strength;
+      console.log('consoleee strength', callNinja.strength);
+      for (var i = 0; i < points; i++) {
+        callNinja.health -= 15;
+      }
+      console.log("In this case, " + callNinja.name + " lost " + points * 15 + " health because " + callNinja.name + " has " + points + " of strength");
     }
     return this;
   }
