95589f8e4766d41c1dfa61af36dacfc370fb76b7
Albiona <albionahh@outlook.com>
2019-04-05 16:55:43 +0200

    feat(02-socket.io): add real time colors

---
+++ 03-express/02-socket.io/03-real-time-colors-optional/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const session = require('express-session');
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(session({
+  secret: 'keyboardkitteh',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}));
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+const server = app.listen(port, () => console.log(`Express server listening on port ${port}!`));
+const io = require('socket.io')(server);
+
+var counter=0;
+
+io.on('connection', function(socket) {
+  socket.emit('greeting', { msg: 'Greeting, from server Node, brought to you by Sockets! - Server'});
+
+  socket.on('green', function(data) {
+    color = data.color;
+    socket.emit('color', { data: color });
+  })
+  socket.on('blue', function(data) {
+    color = data.color;
+    socket.emit('color', { data: color });
+  })
+  socket.on('yellow', function(data) {
+    color = data.color;
+    socket.emit('color', { data: color });
+  })
+
+})
