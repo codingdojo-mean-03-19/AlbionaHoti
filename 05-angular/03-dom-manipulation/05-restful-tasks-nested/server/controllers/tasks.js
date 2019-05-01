b75e492de74d764523ebe2c8c160e9ffbb0ba11a
Albiona <albionahh@outlook.com>
2019-05-01 09:58:53 +0200

    refac(05-angular): add update

---
+++ 05-angular/03-dom-manipulation/05-restful-tasks-nested/server/controllers/tasks.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+Task = mongoose.model('Task');
+module.exports = {
+  tasks: function (_req, res) {
+    console.log('Here --')
+    Task.find({}, function (err, tasks) {
+      if (err) {
+        console.log('Returned error', err);
+        res.json({ message: "Error", error: err });
+      } else {
+        res.json({ message: "Success", tasks: tasks });
+      }
+
+    })
+  },
+
+  getOne: function (req, res) {
+    console.log("ID: ",req.params.id);
+    Task.findById({ _id: req.params.id }, function (err, data) {
+      if (err) {
+        console.log("Returned error", err);
+        res.send({ message: "Error", error: err })
+      } else {
+        res.send({ message: "Success", task: data })
+      }
+    })
+  },
+
+  create: function (req, res) {
+    console.log(req.params.title);
+    Task.create({
+      title: req.params.title
+    })
+    res.send({ message: "Success" })
+  },
+
+  update: function (req, res) {
+    console.log(req.params.id);
+    Task.find({ _id: req.params.id }, function (err, data) {
+      if (err) {
+        console.log('Returned error', err);
+        res.send({ message: "Error", error: err })
+      } else {
+        res.send({ message: "Success", task: data })
+      }
+    })
+  },
+
+  delete: function (req, res) {
+    console.log(req.params.id);
+    Task.delete({ _id: req.params.id }, function (err, data) {
+      if (err) {
+        console.log('Returned error', err);
+        res.send({ message: "Error", data: err })
+      } else {
+        res.send({ message: "Success" })
+      }
+    })
+  },
+}
